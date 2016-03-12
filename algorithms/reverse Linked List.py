33eaa50f6f3bda3a236e167c55e1b781e40b9e32
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-03-11 19:05:00 -0800

    refractor a little bit
    refractor a little bit

--- algorithms/reverse Linked List.py
+++ algorithms/reverse Linked List.py
@@ -3,14 +3,16 @@ class Solution:
     def reverse(self, h):
         if not h: return
         dummy= ListNode(0); dummy.next=  h
-        pre, last, cur = dummy, h, h.next
+        last, cur = h, h.next
         while cur:  #last一直是最后一个。 不变。
             last.next = cur.next
-            cur.next = pre.next
-            pre.next = cur
+            cur.next = dummy.next
+            dummy.next = cur
             cur = last.next   #四层的封闭连环
         return dummy.next
 
 # 每次把cur搬运过来， 到pre后面。 连接pre, pre.next中间
 # dummy作为pre。
 # 虽然不难。 但是过段时间就忘。 不算容易。
+#不需要pre了. dummy就是pre. 
+# dummy 不变， last不变， 只有cur在变化
