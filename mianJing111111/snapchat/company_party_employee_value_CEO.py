72e751c12aed61f8c0f39a445fc59e85c1963af7
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-04-05 17:20:31 -0700

    New problems
    New problems

---
+++ mianJing111111/snapchat/company_party_employee_value_CEO.py
@@ -0,0 +1,45 @@
+'''
+
+
+You are given an organization hierarchy tree (n-ary tree). Every employee (node) has some value (can be -ve or +ve). You have to host a party and have to invite employees such that the total value (summation of each node value) of all the employees is maximum.
+
+there is a rule: no one likes to see their bosses in the party. So you cant invite an employee's immediate boss or subordinate.
+
+You can skip more than 1 level if it gives you maximum value.
+
+
+uppose we are planning a company party.  The company organizational
+structure is so that there is a single Owner who runs the place.
+Everyone has one direct manager, but a manager may have any number of direct.鏈枃鍘熷垱鑷�1point3acres璁哄潧
+reports.  Everyone must report to the owner, possibly indirectly.
+Each employee has associated with him a non-negative “fun” value.  What we
+want to do is invite the set of employees to make the party as fun as
+possible.
+
+Here is the only constraint:  If you invite an employee, you cannot invite
+that employee’s direct manager.
+   A
+B     C
+I J     D  E
+   F G H
+
+If we include A:  total fun value should Fun(A)=sum_{i=I,J,D,E}(Fun(i))
+no A:      Fun(A)={Fun(B)+Fun(C)}. Waral 鍗氬鏈夋洿澶氭枃绔 ,
+It’s legal to invite B and C
+Or it’s legal to invite D, E, A, but you cannot invite D and C, or B and A.
+
+后来复习时才注意到这是party at Hali-Bula,经典树形dp。面试时现推的树形dp，才
+拿到positive feedback。
+
+
+
+
+ Professor Stewart is consulting for the president of a corporation that is planning a company party. The company has a hierarchical structure; that is, the supervisor relation forms a tree rooted at the president. The personnel office has ranked each employee with a conviviality rating, which is a real number. In order to make the party fum for all attendees, the president does not want both an employee and his or her immediate supervisor to attend.
+
+     Professor Stewart is given the tree that describes the structure of the corporation, using theleft-child, right-sibling representation. Each node of the tree holds, in addition to the pointers, the name of an employee and that employee’s conviviality ranking. Describe an algorithm to make up a guest list that maximizes the sum of the conviviality ratings of the guests. Analyze the running time of your algorithm.
+
+   Stewart教授是一家公司总裁的顾问，这家公司计划一个公司聚会。这个公司有一个层次式的结构；也就是说，管理关系形成一棵以总裁为根的树。人事部给每个雇员以喜欢聚会的程度来排名，这是个实数。为了使每个参加者都喜欢这个聚会，总裁不希望一个雇员和他（她）的直接上司同时参加。
+
+   Stewart教授面对一棵描述公司结构的树，使用了左子女、右兄弟表示法。树中每个结点除了包含指针，还包含雇员的名字和该雇员喜欢聚会的排名。描述一个算法，它生成一张客人列表，使得客人喜欢聚会的程度的总和最大。分析你的算法的执行时间。
+
+'''
--- mianJing111111/snapchat/实现下个bloom filter, 听都没听过。实现add(), contains(), 还要实现resize().py
+++ mianJing111111/snapchat/实现下个bloom filter, 听都没听过。实现add(), contains(), 还要实现resize().py
@@ -10,3 +10,99 @@
  
 bloomfilter 是一种数据结构。用到了hashmap和Bit array.
 '''
+
+
+# http://glowingpython.blogspot.com/2013/01/bloom-filter.html
+
+'''
+我面的题目是实现一个bloomfilter
+        设计一个hash函数，根据传入的整数值不同，产生不同hash结果，
+        同一个对象，通过不同的函数映射到多个位置，将这些位置保存为1
+        删除，可在每位上使用计数器
+
+
+
+如果remove的操作并不会有很多的话  可以专门开个hashset之类的东西存已经被remove的element.....   我还以为是很高大上的解法  然而并没有
+
+如果remove的操作并不会有很多的话  可以专门开个hashset之类的东西存已经被remove的element.....   我还以为是很高大上的解法  然而并没有
+
+
+
+if you want to resize a bloom filter secondary storage of keys is required.
+很蠢的芳发把keys都存起来..
+
+'''
+
+class BloomFilter:
+    
+    def __init__(self, size, hashes):
+        self.size = size
+        self.hashes= hashes
+        self.bit_array = [0] * (size)   # array of bits (0, or 1)
+        
+    def add(self, s):
+        for h in self.hashes:
+            self.bit_array[h.hash(s) % self.size] = 1    # %size防止index out
+            
+    def contains(self, string):
+        return all(self.bit_array[h.hash(string) % self.size] for h in self.hashes)
+        
+        
+'''
+        
+size 逐渐增大比较好，可以控制 false positive rate。
+. from: 1point3acres.com/bbs
+比如按 power of 2 增长。
+
+
+加上删除
+class BloomFilter:
+
+    def __init__(self, size, hashes):
+        self.size = size
+        self.hashes= hashes
+        self.bit_array = [0] * (size)   # array of bits (0, or 1)
+        self.count = [0]*(size)
+
+    def add(self, s):
+        for h in self.hashes
+            t = h.hash(s) % self.size
+            self.bit_array[t] = 1    # %size防止index out
+            self.count[t] += 1
+
+    def contains(self, string):
+        return all(self.bit_array[h.hash(string) % self.size] for h in self.hashes)
+
+    def remove(self, string):
+        for h in in self.hashed:
+            t = h.hash(string) % self.size
+            if self.bit_array[t] == 1:
+                self.count[t] -= 1
+                if self.count[t] == 0:
+                    self.bit_array[t] = 0
+
+
+
+
+
+class BloomFilter:
+    
+    def __init__(self, size, hash_count):   
+        self.size = size
+        self.hash_count = hash_count
+        self.bit_array = [0] * (size)   # array of bits (0, or 1)
+        
+    def add(self, string):
+        for seed in xrange(self.hash_count):
+            result = mmh3.hash(string, seed) % self.size   # %size防止index out 
+            self.bit_array[result] = 1
+            
+    def contains(self, string):
+        for seed in xrange(self.hash_count):
+            result = mmh3.hash(string, seed) % self.size
+            if self.bit_array[result] == 0:
+                return False
+        return True
+        
+   
+'''
