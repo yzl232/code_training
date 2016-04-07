21059c132269e88bb45cee40c7b33ae15f2a77de
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-04-07 12:13:25 -0700

    Improved code
    Improved code

--- mianJing111111/geeksforgeeks/bit/Compute modulus division by a power-of-2-number.py
+++ mianJing111111/geeksforgeeks/bit/Compute modulus division by a power-of-2-number.py
@@ -11,3 +11,11 @@ Compute n modulo d without division(/) and modulo(%) operators, where d is a pow
 class Solution:
     def getMod(self, n, d):
         return n&(d-1)
+    
+'''
+Compute n modulo d without division(/) and modulo(%) operators, where d is a power of 2 number.
+
+Let ith bit from right is set in d. For getting n modulus d, we just need to return 0 to i-1 (from right) bits of n as they are and other bits as 0.
+
+For example if n = 6 (00..110) and d = 4(00..100). Last set bit in d is at position 3 (from right side). So we need to return last two bits of n as they are and other bits as 0, i.e., 00..010. 
+'''
--- mianJing111111/geeksforgeeks/bit/Count number of bits to be flipped to convert A to B.py
+++ mianJing111111/geeksforgeeks/bit/Count number of bits to be flipped to convert A to B.py
@@ -2,6 +2,19 @@
 '''
 Count number of bits to be flipped to convert A to B
 '''
+
+class Solution:
+    def cntBits(self, n):
+        cnt = 0
+        while n:
+            cnt+=n&1
+            n>>=1
+        return cnt
+
+    def countFlip(self, a, b):
+        return self.cntBits(a^b)
+        
+'''   
 class Solution:
     def cntBits(self, n):
         cnt=0
@@ -13,7 +26,7 @@ class Solution:
     def countFlip(self, a, b):
         return self.cntBits(a^b)
 
-'''
+
 Solution:
 
   1. Calculate XOR of A and B.
