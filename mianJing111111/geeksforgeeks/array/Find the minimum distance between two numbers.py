70e0c00bf3ececd012bfe42ba87a0bbbf28fe374
yzl232 <yzl232@Zhenglins-Air.PK5001Z>
2016-04-15 16:05:51 -0700

    b
    b

--- mianJing111111/geeksforgeeks/array/Find the minimum distance between two numbers.py
+++ mianJing111111/geeksforgeeks/array/Find the minimum distance between two numbers.py
@@ -1,6 +1,6 @@
 # encoding=utf-8
 '''
-ind the minimum distance between two numbers
+Find the minimum distance between two numbers
 
 Given an unsorted array arr[] and two numbers x and y, find the minimum distance between x and y in arr[]. The array might also contain duplicates. You may assume that both x and y are different and present in arr[].
 
@@ -17,11 +17,13 @@ Output: Minimum distance between 3 and 6 is 4.
 Input: arr[] = {2, 5, 3, 5, 4, 4, 2, 3}, x = 3, y = 2
 Output: Minimum distance between 3 and 2 is 1.
 '''
+
+# leetcode   Shortest Word Distance
 class Solution:
     def findMinD(self, arr, x, y):
-        n = len(arr); ret = n+1;  prev = None  #巧妙，只用了一个prev
+        n = len(arr); ret = n+1;  prev = -1  #巧妙，只用了一个prev
         for i in range(n):
             if arr[i]==x or arr[i]==y:
-                if prev and arr[prev]!=arr[i]:  ret = min(ret, i-prev)
+                if prev!=-1 and arr[prev]!=arr[i]:  ret = min(ret, i-prev)
                 prev =i
         return ret if ret!=n+1 else None
